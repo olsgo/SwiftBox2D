50dc2a4d10543f7452fccbe7656ce810ba01f3b6
olsgo <72028144+olsgo@users.noreply.github.com>
2025-11-09 12:58:47 -0300

    feat: allow excluding heavy geometry extensions

--- Package.swift
+++ Package.swift
@@ -22,7 +22,30 @@ let package = Package(
         ),
         .target(
             name: "SwiftBox2D",
-            dependencies: ["box2d"]
+            dependencies: ["box2d"],
+            exclude: [
+                "Extensions/b2Vec2+Ext.swift",
+                "Extensions/b2Mat22+Ext.swift",
+                "Extensions/b2Rot+Ext.swift",
+                "Extensions/b2Transform+Ext.swift",
+                "Extensions/b2QueryFilter+Ext.swift",
+                "Extensions/b2DebugDraw+Ext.swift",
+                "Extensions/Defs",
+                "Generated/Geometry/B2Vec2+Ext.swift",
+                "Generated/Geometry/B2Mat22+Ext.swift",
+                "Generated/Geometry/B2Rot+Ext.swift",
+                "Generated/Geometry/B2Transform+Ext.swift",
+                "Generated/Geometry/B2Circle+Ext.swift",
+                "Generated/Geometry/B2AABB+Ext.swift",
+                "Generated/B2Capsule+Ext.swift",
+                "Generated/B2Segment+Ext.swift",
+                "Generated/B2QueryFilter+Ext.swift",
+                "Generated/B2HexColor+Ext.swift",
+                "Generated/B2Polygon+Ext.swift",
+                "Generated/B2BodyType+Ext.swift",
+                "Generated/B2ShapeType+Ext.swift",
+                "Generated/B2JointType+Ext.swift"
+            ]
         ),
         .testTarget(
             name: "SwiftBox2DTests",
---
+++ Sources/SwiftBox2D/RothkoAliases.swift
@@ -0,0 +1,19 @@
+import box2d
+
+// Minimal typealiases required by Rothko.
+// These replace the verbose generated extensions that were causing duplicate symbol collisions.
+
+public typealias B2Vec2 = b2Vec2
+public typealias B2Mat22 = b2Mat22
+public typealias B2Rot = b2Rot
+public typealias B2Transform = b2Transform
+public typealias B2Circle = b2Circle
+public typealias B2AABB = b2AABB
+public typealias B2Capsule = b2Capsule
+public typealias B2Segment = b2Segment
+public typealias B2QueryFilter = b2QueryFilter
+public typealias B2HexColor = b2HexColor
+public typealias B2Polygon = b2Polygon
+public typealias B2BodyType = b2BodyType
+public typealias B2ShapeType = b2ShapeType
+public typealias B2JointType = b2JointType
